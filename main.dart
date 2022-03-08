dc027c3cb145cad337b6a27afe366abdc4565ca8
Maryam1720 <99787267+Maryam1720@users.noreply.github.com>
2022-03-08 15:35:05 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new MaterialApp(
+      title: "MARYAM_D0219345",
+      home: new Myapp(),
+    )
+  );
+}
+
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("TUGAS 1 PEMROGRAMAN MOBILE"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(20.0),
+        child: new Center(
+          child: new Text("MARYAM"),
+        )
+      )
+    );
+  }
+}
